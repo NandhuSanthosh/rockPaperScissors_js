48ed0d716581b7fdf871137d370897eb8be56fed
Nandhu Santhosh <nandhusanthosh87@gmail.com>
2022-08-13 15:29:12 +0530

    animations are added

--- javaScript.js
+++ javaScript.js
@@ -26,7 +26,14 @@ house_picked = `<div class="selection-details">
 <span>THE HOUSE PICKED</span>
 </div>`
 
-score = 0
+rules_content = `<div class="rules-tab">
+<div class="rules-title">
+  <p class="rules-title-text">RULES</p>
+ </div>
+</div>`
+
+your_score= 0
+com_socre = 0
 
 symbols = [{
         'code': paper_symbol, 
@@ -41,6 +48,8 @@ symbols = [{
 
 
 score_container = document.getElementById('score-value')
+rules_button = document.getElementById('rules-button')
+rules_tab = document.getElementById('rules-tab')
 one = document.getElementById('holder-one')
 two = document.getElementById('holder-two')
 three = document.getElementById('holder-three')
@@ -59,7 +68,6 @@ function onclick_adder(){
         item['position'].addEventListener('click', (Event)=>{
             ele = Event.path.reverse()[9]
             selection = ele.dataset.id
-            console.log(selection)
             after_selected(selection)
         })
     })
@@ -75,28 +83,63 @@ function after_selected(selection){
         computer_choice = symbols[rand()]
         holders[1]['position'].innerHTML = computer_choice['code']
         holders[2]['position'].style.display = 'none'
-        console.log(holders)
         selection_details_printer()
-        score_updater(computer_choice['id'], selection)
-        dly()
+        winner = score_updater(computer_choice['id'], selection)
+        animation_adder(winner)
+        
+}
+function animation_adder(winner) {
+    if (winner != -1){
+        holders[winner]['position'].classList.add('winner_shadow')
+    }
+    dly(winner)
+}
+function animation_remover(winner) {
+    if (winner != -1){
+        holders[winner]['position'].classList.remove('winner_shadow')
+    }
 }
 
 function score_updater(computer_choice, selection) {
     console.log(selection, computer_choice )
     if(computer_choice == 'rock' & selection == 'paper'){
-        score +=1
+        your_score+=1
+        winner = 0
     }else if( computer_choice == 'paper' & selection == 'scissor'){
-        score +=1
+        your_score+=1
+        winner = 0
     }else if( computer_choice == 'scissor' & selection == 'rock'){
-        score += 1
+        your_score+= 1
+        winner = 0
+    }else if( computer_choice != selection){
+        winner = 1
+    }
+    else{
+        winner = -1
+        com_score += 1;
     }
     score_container.innerHTML = score
+    return winner
 }
 
-async function dly(){
+async function dly(winner){
         await sleep(2000);  
-        strater()
+        if (your_score == 10){
+            result_printer('YOU WIN')
+        }
+        else if(com_score == 10){
+            result_printer('YOU LOSE')
+        }
+        else{
+            strater()
+            animation_remover(winner)
+        }
 }
+
+function result_printer(string){
+
+}
+
 function sleep(milliseconds) {  
     return new Promise(resolve => setTimeout(resolve, milliseconds));  
  }  
@@ -131,5 +174,15 @@ function strater(){
         item['content']= symbols[index]['id']
     })
 }
+function rules_display(){
+    rules_tab.style.display='block'
+    rules_tab.innerHTML = rules_content
+}
+function rules_hidder() {
+    rules_tab.style.display='none'
+}
+
+rules_button.addEventListener('click', rules_display)
+rules_tab.addEventListener('click', rules_hidder)
 strater()
 onclick_adder()
